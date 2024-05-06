ba315f5018faf125a097992f7fb39921bb402d77
alberto-it <alberto.it@gmail.com>
2024-05-06 09:54:16 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Pokédex
+This is a simple web application that functions as a Pokédex, allowing users to search for Pokémon by ID or name and display detailed information about them.
+
+## Technologies Used:
+* HTML: Provides the basic structure of the web page.
+* CSS: Styles the layout and appearance of the page.
+* JavaScript: Adds interactivity and functionality to the application.
+* Bootstrap: Used for responsive design and pre-built components.
+* PokéAPI: An external API used to retrieve Pokémon data.
+# Files:
+*index.html: The main HTML file that displays the search bar, results area, and custom messages.
+* script.js: Contains the JavaScript code for handling user interactions, fetching Pokémon data, and displaying information.
+* style.css: Defines the styles for various elements like the header, cards, and messages.
+## Functionality:
+* Users can search for Pokémon by entering their ID number (1-1025) or name in the search bar.
+* Clicking the "View" button triggers the search process.
+* Clicking the "Clear" button resets the search and removes any displayed information.
+* Upon successful search, the following information is displayed:
+    * Pokémon image
+    * Name
+    * ID number (in the "Added" custom message)
+    * Types
+    * Abilities
+    * Stats (HP, Attack, Defense, etc.)
+* Custom messages are displayed based on the search result:
+    * A custom "Added" message appears when a new Pokémon is successfully added.
+    * "Pokédmon Not Found 😢" message appears when a Pokémon cannot be found.
+    * A custom "Already There" message appears when a previously added Pokémon is searched again.
+## Additional Notes:
+* The application utilizes the PokéAPI to retrieve Pokémon data.
+* The code includes basic animation for the displayed Pokémon cards.
+* The provided CSS includes responsive styles for smaller screen sizes for mobile devices.
+
+This README.md provides a basic overview of the Pokédex web application and its functionalities. Feel free to explore the code further to understand the implementation details.
+
+GitHub Repository: https://github.com/alberto-it/API-Pokemon
---
+++ script.js
@@ -0,0 +1,112 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const mainContent = document.getElementById('mainContent');
+
+searchInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        searchButton.click();
+    }
+});
+
+clearButton.addEventListener('click', () => {
+    mainContent.innerHTML = '';
+    searchInput.value = '';
+    seenPokemon = [];
+});
+
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm.length !== 0) {
+        fetchPokemon(searchTerm);
+    }
+});
+
+let seenPokemon = [];
+
+async function fetchPokemon(searchTerm) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error fetching Pokemon: ${response.statusText}`);
+        }
+        const data = await response.json();
+
+        if (seenPokemon.includes(data.id)) {
+            const alreadyThereMsg = document.getElementById('alreadyThereMsg');
+            alreadyThereMsg.textContent = `${data.name.toUpperCase()} Already Added!`;
+            alreadyThereMsg.classList.remove('d-none');
+            setTimeout(() => {
+                alreadyThereMsg.classList.add('d-none');
+            }, 2500);
+            searchInput.value = '';
+        } else {
+            seenPokemon.push(data.id);
+            displayPokemonDetails(data);
+        }
+    } catch (error) {
+        console.log(error);
+        notFoundMessage.classList.remove('d-none');
+        setTimeout(() => {
+            notFoundMessage.classList.add('d-none');
+        }, 3500);
+    }
+}
+
+const successMessage = document.getElementById('successMessage');
+
+function displayPokemonDetails(pokemon) {
+    const successMessage = document.getElementById('successMessage');
+    successMessage.textContent = `Adding #${seenPokemon.length} - ${pokemon.name.toUpperCase()} (ID: ${pokemon.id})`;
+
+    successMessage.classList.remove('d-none');
+    setTimeout(() => {
+        successMessage.classList.add('d-none');
+    }, 2500);
+
+    const card = document.createElement('div');
+    card.classList.add('card', 'mb-2');
+
+    const img = document.createElement('img');
+    img.classList.add('card-img');
+    img.src = pokemon.sprites.front_default;
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const title = document.createElement('p');
+    title.classList.add('card-title');
+    title.textContent = pokemon.name.replace(/^./, match => match.toUpperCase());
+
+    const types = document.createElement('p');
+    types.classList.add('pokemon-types');
+
+    const abilities = document.createElement('p');
+    abilities.classList.add('pokemon-abilities');
+
+    types.textContent = `types: ${pokemon.types.map(type => type.type.name).join(', ')}`.toUpperCase();
+    abilities.textContent = `abilities: ${pokemon.abilities.map(ability => ability.ability.name).join(', ')}`.toUpperCase();
+
+    const stats = document.createElement('ul');
+    stats.classList.add('pokemon-stats');
+
+    pokemon.stats.forEach(stat => {
+        const statItem = document.createElement('li');
+        statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+        stats.appendChild(statItem);
+    });
+
+    card.classList.add('card-enter'); // Animation Effect (see bottom of CSS)
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(types);
+    cardBody.appendChild(abilities);
+    cardBody.appendChild(stats);
+
+    card.appendChild(img);
+    card.appendChild(cardBody);
+
+    mainContent.appendChild(card);
+
+    searchInput.value = '';
+}
